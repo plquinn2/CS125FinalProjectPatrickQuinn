5b74308f2f727afb935fd978181f2727f271f3f9
Patrick Quinn <plquinn01@gmail.com>
2019-04-30 16:10:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cs125finalprojectpatrickquinn/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.cs125finalprojectpatrickquinn;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private static final String TAG = "MainActivity";
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() : started.");
+        ImageView firstimage = (ImageView) findViewById(R.id.firstimage);
+
+        int imageResource = getResources().getIdentifier("@drawable/shaq", null, this.getPackageName());
+        firstimage.setImageResource(imageResource);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
---
+++ app/src/main/java/com/example/cs125finalprojectpatrickquinn/UserInterfaceRecognition.java
@@ -0,0 +1,4 @@
+package com.example.cs125finalprojectpatrickquinn;
+
+public class UserInterfaceRecognition {
+}
